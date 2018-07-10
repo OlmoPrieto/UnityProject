d5558a04e3c1b45e32d357748d9dd2754dd46376
OlmoPrieto <olmo.prieto@mybrana.com>
2018-07-10 18:13:05 +0200

    Advanced on player's movement, added skeleton's running and attack animations

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -54,44 +54,74 @@ public class PlayerMovement : MonoBehaviour {
     m_rigidbody.velocity = velocity;
   }
 
+  private void setWallsFlags(Collision2D other) {
+    m_touching_wall = true;
+
+    // Check in which side of a wall you are
+    if (transform.position.x < other.transform.position.x) {
+      m_touching_right = true;
+      m_touching_left = false;
+      if (m_x_wall_force > 0) {
+        m_x_wall_force = -m_x_wall_force;
+      }
+    }
+    else {
+      m_touching_right = false;
+      m_touching_left = true;
+      if (m_x_wall_force < 0) {
+        m_x_wall_force = -m_x_wall_force;
+      }
+    }
+  }
+
   void OnCollisionEnter2D(Collision2D other) {
+    ContactPoint2D[] contacts = new ContactPoint2D[4];
+    int num_contacts = other.GetContacts(contacts);
+
     m_was_walljumping = false;
 
+    // Floors
     if (other.collider.gameObject.layer == 9) {
       m_grounded = true;
+
+      if (contacts[0].point.x == contacts[1].point.x) {
+        // Touched the left/right part of a floor,
+        // behave like in a wall
+        setWallsFlags(other);
+      }
     }
 
+    // Walls
     if (other.collider.gameObject.layer == 10) {
-      m_touching_wall = true;
-
-      if (transform.position.x < other.transform.position.x) {
-        m_touching_right = true;
-        m_touching_left = false;
-        if (m_x_wall_force > 0) {
-          m_x_wall_force = -m_x_wall_force;
-        }
-      }
-      else {
-        m_touching_right = false;
-        m_touching_left = true;
-        if (m_x_wall_force < 0) {
-          m_x_wall_force = -m_x_wall_force;
-        }
-      }
+      setWallsFlags(other);
 
       m_grounded = true;
+
+      if (contacts[0].point.y == contacts[1].point.y) {
+        // Touched the upper/lower part of a wall,
+        // behave like in the floor
+        m_touching_wall = false;
+        m_touching_right = false;
+        m_touching_left = false;
+      }
+    }
+
+    for (int i = 0; i < num_contacts; ++i) {
+      Debug.Log("x: " + contacts[i].point.x + "  y: " + contacts[i].point.y);
     }
   }
 
   void OnCollisionExit2D(Collision2D other) {
+    // Floors
     if (other.collider.gameObject.layer == 9) {
       m_grounded = false;
     }
 
+    // Walls
     if (other.collider.gameObject.layer == 10) {
       m_grounded = false;
+      
       m_touching_wall = false;
-
       m_touching_right = false;
       m_touching_left = false;
     }
@@ -101,7 +131,7 @@ public class PlayerMovement : MonoBehaviour {
     yield return new WaitForSeconds(0.2f);
     //m_can_jump = true;
     m_was_walljumping = false;
-    m_rigidbody.velocity = new Vector2(m_rigidbody.velocity.x * 0.0f, 
+    m_rigidbody.velocity = new Vector2(0.0f /*m_rigidbody.velocity.x * 0.2f*/, 
       m_rigidbody.velocity.y);
   }
 }
