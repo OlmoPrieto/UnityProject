f61cc51f7ad887d492c65c10909ecd1b9c32f9c6
OlmoPrieto <olmo.prieto@mybrana.com>
2018-07-11 16:14:07 +0200

    Added missing files

---
+++ Project/Assets/Scripts/SkeletonAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAnimation : MonoBehaviour {
+
+  public Animator m_animator;
+
+  public float m_change_animation_time = 5.0f;
+  public float m_acc_time = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+    m_acc_time = Random.value * 2.0f;
+		m_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_acc_time += Time.deltaTime;
+    if (m_acc_time >= m_change_animation_time) {
+      m_acc_time = Random.value * 1.5f;
+
+      m_animator.SetTrigger("Attacking");
+    }
+	}
+}
