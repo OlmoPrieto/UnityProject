eed7a5021fd582edbdccb146e84303dd6e8dbf1d
OlmoPrieto <olmo.prieto@mybrana.com>
2018-07-12 12:59:16 +0200

    Added Explosion (name WIP) prefab and tested how the bones will 'explode'. Also added Die animation for skellis

---
+++ Project/Assets/Scripts/Explosion.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+  public Rigidbody2D m_rigidbody;
+  public Transform m_transform;
+  public Vector2 m_direction = new Vector2();
+  public float m_impulse = 1.0f;
+  public float m_torque = 1.0f;
+  public bool m_freeze_gravity = true;
+  public bool m_physics_enabled = true;
+
+	// Use this for initialization
+	void Start () {
+		m_rigidbody = GetComponent<Rigidbody2D>();
+    m_rigidbody.gravityScale = 0.0f;
+    m_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (m_freeze_gravity == true) {
+      m_rigidbody.velocity = new Vector2(0.0f, 0.0f);
+    }
+
+    if (m_physics_enabled == true && 
+      (m_rigidbody.velocity.x == 0.0f && m_rigidbody.velocity.y == 0.0f)) {
+      m_physics_enabled = false;
+
+      m_rigidbody.Sleep();
+    }
+
+		if (Input.GetMouseButtonDown(0)) {
+      m_freeze_gravity = false;
+      m_rigidbody.gravityScale = 1.0f;
+
+      float random = Random.value * 2.0f - 0.5f;
+      m_direction.y = random;
+
+      m_rigidbody.AddForce(m_direction * m_impulse, ForceMode2D.Impulse);
+      m_rigidbody.AddTorque(m_torque, ForceMode2D.Impulse);
+    }
+	}
+
+  void OnCollisionEnter2D(Collision2D other) {
+    if (other.collider.gameObject.tag == "Player") {
+      Destroy(this.gameObject);
+    }
+  }
+}
